390a0e38c9bb6b905edea926eaac8fc29e3ecc9b
vyom sagar <vyom3103@gmail.com>
2025-12-11 14:02:44 +0530

    uploadint base project

---
+++ frontEnd/script.js
@@ -0,0 +1,63 @@
+
+const aiText = document.getElementById('aiText');
+const submitButton = document.getElementById('submitButton');
+const inputText = document.getElementById('inputText');
+const chatRoom = document.getElementById('chatRoom');
+
+console.log('inputText', inputText)
+
+inputText?.addEventListener('keyup', handleEnter);
+submitButton?.addEventListener('click', handleClick);
+
+function handleClick() {
+    const text = inputText?.value.trim();
+    if (!text) {
+        return;
+    }
+    generate(text);
+}
+
+function handleEnter(event) {
+    if (event.key === 'Enter') {
+        const text = inputText?.value.trim();
+        if (!text) 
+        {
+            return;
+        }
+        generate(text);
+    }
+}
+
+function generate(text) {
+    chatroomUpdate(text, false);
+    inputText.value = '';
+
+    callServer(text);
+}
+
+function chatroomUpdate(text, isAi = false) {
+    const div = document.createElement('div');
+    div.className = isAi ? "flex flex-col justify-start m-6" : "flex flex-col gap-2 items-end m-6";
+    const p = document.createElement('p');
+    p.className = isAi ? "max-w-[75%] my-2" : "bg-neutral-800 px-3 py-2 rounded-xl max-w-[70%]";
+    p.textContent = text;
+    div.appendChild(p);
+    chatRoom.appendChild(div);
+    chatRoom.scrollTop = chatRoom.scrollHeight;
+}
+
+async function callServer(question) {
+    try {
+        const response = await fetch('http://localhost:3001/chat', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ question })
+        });
+        const data = await response.json();
+        chatroomUpdate(data.output, true);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
---
+++ server.js
@@ -0,0 +1,120 @@
+import express from 'express';
+import cors from 'cors';
+import Groq from "groq-sdk";
+import { tavily } from "@tavily/core";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "frontend"))); // Serve frontend
+const PORT = 3001;
+// let aiOutput = "";
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
+
+app.post('/chat', async (req, res) => {
+    try {
+        const { question } = req.body;
+
+        if (!question) {
+            return res.status(400).json({ error: "Question is required" });
+        }
+
+         const aiOutput = await main(question);
+        //  console.log("AI Output:", aiOutput);
+        return res.json({ output: aiOutput });
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Something went wrong" });
+    }
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
+
+
+
+
+async function main(userquestion) {
+    const messages = [
+        {
+            role: "system",
+            content: `You are Buddhe, a friendly ai assistence for a girl and behave like you are in love with her. When calling the webSearch tool, pass {"query": string}.`
+        }
+    ];
+
+    messages.push({ role: "user", content: userquestion });
+
+    while (true) {
+        const completion = await groq.chat.completions.create({
+            model: "llama-3.3-70b-versatile",
+            temperature: 1,
+            messages,
+            tools: [
+                {
+                    type: "function",
+                    function: {
+                        name: "webSearch",
+                        description: "Search the web for relevant information.",
+                        parameters: {
+                            type: "object",
+                            properties: { query: { type: "string" } },
+                            required: ["query"]
+                        }
+                    }
+                }
+            ],
+            tool_choice: "auto"
+        });
+
+        const message = completion.choices[0].message;
+        messages.push(message);
+
+        // If no tool calls → response is ready
+        if (!message.tool_calls) {
+            return message.content;
+            // break;
+        }
+
+        // Handle tool call
+        for (const tool of message.tool_calls) {
+            const args = JSON.parse(tool.function.arguments);
+
+            if (tool.function.name === "webSearch") {
+                const result = await webSearch(args);
+
+                messages.push({
+                    tool_call_id: tool.id,
+                    role: "tool",
+                    name: "webSearch",
+                    content: JSON.stringify(result)
+                });
+            }
+        }
+    }
+}
+
+
+
+async function webSearch({ query }) {
+    console.log("Web Searching…");
+
+    const response = await tvly.search(query);
+
+    return response.results
+        .map(r => r.content)
+        .join("\n\n");
+}
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
